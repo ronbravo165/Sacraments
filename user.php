898e79f4b33c66a2d79b02348a5866cccec99e81
antonio.bravo <13165318+MrSimplevia2018@users.noreply.github.com>
2024-09-22 20:03:17 +0800

    Update user.php
    Update user.php

--- user.php
+++ user.php
@@ -33,36 +33,106 @@ if (isset($_POST['add'])) {
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link href="bootstrap.min.css" rel="stylesheet" type='text/css'>
 	<link rel="icon" type="image/png" href="images/logo.png">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="stylesheet" type="text/css" href="css/style2.css">
-	<link rel="stylesheet" type="text/css" href="css/style3.css">
+	<link rel="stylesheet" type="text/css" href="css/heading.css">
+	<link rel="stylesheet" href="jquery-ui.css">
+	<link href="fonts/font-awesome.min.css" rel="stylesheet" type='text/css'>
+	<!-- Data Table CSS -->
+	<link rel='stylesheet' href='dataTables.bootstrap5.min.css'>
+	<script src="bootstrap.bundle.min.js" ></script>
+	<script src="popper.min.js"></script>
+	<script src="bootstrap.min.js"></script>
+	
+	<!-- jQuery -->
+	<script src='jquery-3.7.0.js'></script>
+	<script src="jquery-1.12.4.js"></script>
+	<script src="jquery-ui.js"></script>
+	<!-- Data Table JS -->
+	<script src='jquery.dataTables.min.js'></script>
+	<script src='dataTables.responsive.min.js'></script>
+	<script src='dataTables.bootstrap5.min.js'></script>
 	<title>SRS - Deceased</title>
 </head>
 <body>
 
-	<div class="header">
-		<img src="images/logo.png">
-		<h1>Sacramental Record System</h1>
-		<h3>Parokya ni San Nicolas de Tolentino</h3>
+<?php include 'connection.php'; ?>
+	<?php include 'topbar.php'; ?>
+
+	<div class="container-fluid">
+		<div class="row flex-nowrap">
+			<div id="menu" class="d-flex flex-column vh-100 flex-shrink-0 p-3 text-white bg-dark" style="width: 250px;"> 
+				<ul class="nav nav-pills flex-column mb-auto"> 
+					<li> 
+						<a href="home.php" class="nav-link text-white" aria-current="page"> <i class="fa fa-home"></i><span class="ms-2">Home</span> </a> 
+					</li> 
+					<li>
+						<a href="baptism.php" class="nav-link text-white"> <i class="fa fa-first-order"></i><span class="ms-2">Baptism</span> </a> 
+					</li>
+					<li>
+						<a href="communion.php" class="nav-link text-white"> <i class="fa fa-first-order"></i><span class="ms-2">Communion</span> </a> 
+					</li> 
+					<li> 
+						<a href="confirmation.php" class="nav-link text-white"> <i class="fa fa-cog"></i><span class="ms-2">Confirmation</span> </a> 
+					</li> 
+					<li> 
+						<a href="wedding.php" class="nav-link text-white"> <i class="fa fa-bookmark"></i><span class="ms-2">Wedding</span> </a> 
+					</li>
+					<li> 
+						<a href="deceased.php" class="nav-link text-white"> <i class="fa fa-bookmark"></i><span class="ms-2">Deceased</span> </a> 
+					</li> 
+					<li > 
+						<a href="requests.php" class="nav-link text-white"> <i class="fa fa-bookmark"></i><span class="ms-2">Request</span> </a> 
+					</li>
+					<li class="active"> 
+						<a href="user.php" class="nav-link text-white"> <i class="fa fa-bookmark"></i><span class="ms-2">User Lists</span> </a> 
+					</li>
+				</ul> <hr> 
+			</div>
+			<div class="col py-3">
+				<h1 align="center">User Lists</h1>
+				<table id="example" class="table table-striped example" style="width:100%">
+					<thead>
+						<tr>
+							<td>Full name</td>
+							<td>Username</td>
+							<td>Action</td>
+						</tr>
+					</thead>
+					
+					<tbody>
+						<?php include 'connection.php';
+								$result = mysqli_query($con,"SELECT * FROM user_tbl"); 
+								while ($row = $result->fetch_assoc()):
+						?>
+						<tr>
+							<td><?php echo $row['fullname']; ?></td>
+							<td><?php echo $row['username']; ?></td>
+							<td>
+							<ul class="list-inline m-0">
+									<li class="list-inline-item">
+										<button class="btn btn-success btn-sm rounded-0" type="button" title="Edit"><i class="fa fa-edit"></i></button>
+									</li>
+									<li class="list-inline-item">
+										<a class="btn btn-danger btn-sm rounded-0" title="Delete" href='baptismdelete.php?id="<?php echo $row['id']; ?>"'><i class="fa fa-trash"></i></a>
+									</li>
+								</ul>
+								<!-- <a style='text-decoration: none; color: black;' href='baprequestdelete.php?id="<?php echo $row['id']; ?>"'>&nbsp;&nbsp;Delete&nbsp;&nbsp;</a> -->
+							
+							</td>
+						</tr>
+
+						<?php endwhile; ?>
+					</tbody>
+					
+				</table>
+			</div>
+		</div>
 	</div>
 
-	<ul>
-		<li><a href="#" class="nav-menu" style="background-color: gold;"></a></li>
-		<li><a href="home.php" class="nav-menu">Home</a></li>
-		<li><a href="baptism.php" class="nav-menu">Baptism</a></li>
-		<li><a href="communion.php" class="nav-menu">Communion</a></li>
-		<li><a href="confirmation.php" class="nav-menu">Confirmation</a></li>
-		<li><a href="wedding.php" class="nav-menu">Wedding</a></li>
-		<li><a href="deceased.php" class="nav-menu">Deceased</a></li>
-		<li><a href="requests.php" class="nav-menu">Requests</a></li>
-		<li><a href="user.php" class="nav-menu active">User Lists</a></li>
-		<li><a href="logout.php" class="nav-menu" style="color: red">LOGOUT</a></li>
-	</ul>
-
-<?php
+<!-- <?php
 
 	include 'connection.php';
 	$result = mysqli_query($con,"SELECT * FROM user_tbl");
@@ -91,7 +161,34 @@ if (isset($_POST['add'])) {
 
 		<p align="center">Diocese of San Jose Nueva Ecija<br>Parokya ni San Nicolas de Tolentino<br>Carranglan, Nueva Ecija<br>&copy; All rights reserved 2024.</p>
 		
-	</footer>
+	</footer> -->
 
 </body>
+<script>
+
+	$(document).ready(function() {
+		$('.example').DataTable({
+		//disable sorting on last column
+		"columnDefs": [
+			{ "orderable": false, "targets": 2 }
+		],
+		language: {
+			//customize pagination prev and next buttons: use arrows instead of words
+			'paginate': {
+			'previous': '<span class="fa fa-chevron-left"></span>',
+			'next': '<span class="fa fa-chevron-right"></span>'
+			},
+			//customize number of elements to be displayed
+			"lengthMenu": 'Display <select class="form-control input-sm">'+
+			'<option value="10">10</option>'+
+			'<option value="20">20</option>'+
+			'<option value="30">30</option>'+
+			'<option value="40">40</option>'+
+			'<option value="50">50</option>'+
+			'<option value="-1">All</option>'+
+			'</select> results'
+		}
+		})  
+	});
+</script>
 </html>
